7e78fdcf116616898110873cbc1ca17e93d07b2e
Sangu1nine <hj990406@naver.com>
2025-03-18 16:01:39 +0900

    asd

--- function.py
+++ function.py
@@ -85,16 +85,17 @@ def delete_product(conn, product_id):
         print(error)
     return affected_rows
 
+def search_product
 if __name__ == '__main__':
     print(__name__)
     print(read_config())
     conn = connect()
-    product_name = input('상품명을 입력하세요>>>')
-    barcode = input('바코드를 입력하세요>>>')
-    price = int(input('가격을 입력하세요>>>'))
-    quantity = int(input('재고 수량을 입력하세요>>>'))
-    expiration_day = input('유통기한을 입력하세요>>>')
-    insert_product(conn, product_name, barcode, price, quantity, expiration_day)
+    # product_name = input('상품명을 입력하세요>>>')
+    # barcode = input('바코드를 입력하세요>>>')
+    # price = int(input('가격을 입력하세요>>>'))
+    # quantity = int(input('재고 수량을 입력하세요>>>'))
+    # expiration_day = input('유통기한을 입력하세요>>>')
+    # insert_product(conn, product_name, barcode, price, quantity, expiration_day)
     # affected_rows = update_product(conn, 1, '새로운 상품명', 2000, 50) # id가 1인 상품의 정보를 변경
     # print(f'변경된 행 수: {affected_rows}')
     query_with_fetchall(conn)
